53537f3e44331731cc87584db75243997baefd21
ModerrOn <sensansion@gmail.com>
2024-08-16 18:05:47 +0500

    Добавьте файлы проекта.

---
+++ Entity/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Shop.Domain.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string NameProduct { get; set; }
+        public int IdNameCategory { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string NameCategory { get; set; }       
+    }
+
+    public class ResultJoin
+    {
+        [Key]
+        public string NameProduct { get; set; }
+        public string NameCategory { get; set; }
+    }
+}
